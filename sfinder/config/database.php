1f28e7c7de2bf4a0e15937812531ef33c3a7e2ef
Jens U. Dittrich <jdittrich@gmail.com>
2011-05-10 10:05:29 +0200

    Initial commit t

---
+++ sfinder/config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysqli',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'cakeapp',
+		'password' => 'cake1946',
+		'database' => 'schlaglochfinder',
+		'encoding' => 'utf8'
+	);
+}
+?>
